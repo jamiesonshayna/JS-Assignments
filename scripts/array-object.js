05b41ddf49260a8d6c740301cf9206ef7e7a3d91
Shayna Jamieson <jamieson.shayna@gmail.com>
2020-01-22 16:13:56 -0800

    Add initial project 4 files

---
+++ scripts/array-object.js
@@ -0,0 +1,10 @@
+/*
+@author: Shayna Jamieson
+@version: 1.0
+Date: January 22, 2020
+File Name: array-object.js
+Description: This file contains a function that accepts an array as a parameter.
+The function returns an object. The function converts the array to an object with
+properties containing the array values, grouped by type.
+ */
+
